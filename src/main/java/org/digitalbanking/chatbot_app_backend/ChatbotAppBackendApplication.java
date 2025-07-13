6a1f9da4905a033a5c7f128f8bc4b2bdce5885a4
BARNOCH Anas <anasbarnoch@gmail.com>
2025-07-13 16:28:52 +0100

    Init project

---
+++ src/main/java/org/digitalbanking/chatbot_app_backend/ChatbotAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.digitalbanking.chatbot_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/digitalbanking/chatbot_app_backend/ChatbotAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.digitalbanking.chatbot_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

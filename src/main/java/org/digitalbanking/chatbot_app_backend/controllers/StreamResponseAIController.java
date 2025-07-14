26f068610e65a901a4d373439522e993653f39dc
BARNOCH Anas <anasbarnoch@gmail.com>
2025-07-14 12:56:59 +0100

    Stream Response

--- src/main/java/org/digitalbanking/chatbot_app_backend/controllers/StreamResponseAIController.java
+++ src/main/java/org/digitalbanking/chatbot_app_backend/controllers/StreamResponseAIController.java
@@ -1,33 +1,29 @@
 package org.digitalbanking.chatbot_app_backend.controllers;
 
-import org.digitalbanking.chatbot_app_backend.outputs.ListMovies;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import reactor.core.publisher.Flux;
 
 @RestController
-public class StructuredResponseAIController {
+public class StreamResponseAIController {
     private final ChatClient chatClient;
 
-    public StructuredResponseAIController(ChatClient.Builder builder, MessageWindowChatMemory memory) {
+    public StreamResponseAIController(ChatClient.Builder builder, MessageWindowChatMemory memory) {
         this.chatClient = builder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(memory).build())
                 .build();
     }
 
-    @GetMapping("structuredResponse/chat")
-    public ListMovies chat(String text) {
-        String systemMessage = "You are a cinema specialist";
+    @GetMapping("streamResponse/chat")
+    public Flux<String> chat(String text) {
         return chatClient.prompt()
-                .system(systemMessage)
                 .user(text)
-                .call()
-                .entity(ListMovies.class);
+                .stream()
+                .content();
     }
 }
